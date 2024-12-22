084dd2fe7f8074d984d2aa01eed029cd6fd76a7d
KARTHIK MOHAN <91784624+KarthikMohan7639@users.noreply.github.com>
2024-12-22 17:16:31 +0530

    Update S21PP.java

--- PatternProgramming/S21PP.java
+++ PatternProgramming/S21PP.java
@@ -12,9 +12,7 @@ public class S21PP {
         {
         for(j=0;j<n;j++) //columns
         {
-            if(i<=j){
-                
-            }
+            
             System.out.print("* ");
             }
             System.out.println();
